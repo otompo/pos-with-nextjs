4aa107bce84f6daa07f65c99c7c3e2759754a09c
Abagna <abagnasalifusasco@gmail.com>
2022-11-17 09:08:21 -0200

    latest commit

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    DB_URL:
-      'mongodb+srv://sasco:oAPRV35UNyWLorpF@cluster0.fpvql.mongodb.net/posdb?retryWrites=true&w=majority',
-    DB_LOCAL: 'mongodb://localhost:27017/posdb',
-    JWT_SECRET: 'sfskftsfdssdsp3405059o53H530smdslf',
-    JWT_COOKIE_EXPIRES_IN: '50',
-    AWS_BUCKET: 'afrotalian-bucket',
-    AWS_ACCESS_KEY_ID: 'AKIAZQJ5SNLB3NQVBH42',
-    AWS_SECRET_ACCESS_KEY: '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',
-    AWS_REGION: 'us-west-2',
-    AWS_API_VERSION: '2012-10-17',
-    NEXTAUTH_URL: 'https://pos.com',
+    DB_URL: '',
+    JWT_SECRET: '',
+    JWT_COOKIE_EXPIRES_IN: '',
+    NEXTAUTH_URL: '',
   },
 
   eslint: {
